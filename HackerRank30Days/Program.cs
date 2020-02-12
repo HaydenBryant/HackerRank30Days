55292173340ecf38411cc33775559c01bd2aa7ba
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-02-12 15:33:47 -0500

    Day1 Done

--- HackerRank30Days/Program.cs
+++ HackerRank30Days/Program.cs
@@ -1,11 +1,32 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
+using System.IO;
 
-class Difference
+class Solution
 {
-    private int[] elements;
-    public int maximumDifference;
+    static void Main(String[] args)
+    {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
 
-    // Add your code here
 
-} // End of Difference Class
+        // Declare second integer, double, and String variables.
+        int numInt;
+        double numDbl;
+        string inputStr;
+        // Read and save an integer, double, and String to your variables.
+        numInt = Convert.ToInt32(Console.ReadLine());
+        numDbl = Convert.ToDouble(Console.ReadLine());
+        inputStr = Console.ReadLine();
+
+        // Print the sum of both integer variables on a new line.
+        Console.WriteLine(numInt + i);
+        // Print the sum of the double variables on a new line.
+        Console.WriteLine(string.Format("{0:0.0}", numDbl + d));
+        // Concatenate and print the String variables on a new line
+        // The 's' variable above should be printed first.
+        Console.WriteLine(s + inputStr);
+
+    }
+}
