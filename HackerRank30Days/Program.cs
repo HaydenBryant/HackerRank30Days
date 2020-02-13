86967e0f920a1557b48c79a56f71c98536e7fb48
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-02-13 11:28:33 -0500

    Day4 done

--- HackerRank30Days/Program.cs
+++ HackerRank30Days/Program.cs
@@ -1,44 +1,44 @@
-﻿using System.CodeDom.Compiler;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
 
-class Solution
+class Person
 {
-
-
-
-    static void Main(string[] args)
+    public int age;
+    public Person(int initialAge)
     {
-        int N = Convert.ToInt32(Console.ReadLine());
-
-        if (N % 2 == 0)
+        // Add some more code to run some checks on initialAge
+        if (initialAge < 0)
         {
-            if (N >= 2 && N <= 5)
-            {
-                Console.WriteLine("Not Weird");
-            }
-            if (N >= 6 && N <= 20)
-            {
-                Console.WriteLine("Weird");
-            }
-            if (N > 20)
-            {
-                Console.WriteLine("Not Weird");
-            }
+            Console.WriteLine("Age is not valid, setting age to 0.");
+            age = 0;
         }
         else
         {
-            Console.WriteLine("Weird");
+            age = initialAge;
         }
     }
-}
+    public void amIOld()
+    {
+        // Do some computations in here and print out the correct statement to the console 
+        if (age < 13)
+        {
+            Console.WriteLine("You are young.");
+        }
+        else if (age >= 13 && age < 18)
+        {
+            Console.WriteLine("You are a teenager.");
+        }
+        else
+        {
+            Console.WriteLine("You are old.");
+        }
+    }
+
+    public void yearPasses()
+    {
+        // Increment the age of the person in here
+        age++;
+    }
+
+    static void Main(String[] args) {
