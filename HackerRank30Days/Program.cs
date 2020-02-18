56c0c61792b49e177f0866334342041dcb339224
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-02-17 22:22:14 -0500

    ay 3

--- HackerRank30Days/Program.cs
+++ HackerRank30Days/Program.cs
@@ -19,59 +19,26 @@ class Solution
 
     static void Main(string[] args)
     {
-        int n = Convert.ToInt32(Console.ReadLine());
-    }
-}
+        int N = Convert.ToInt32(Console.ReadLine());
 
-//Day 6
-using System;
-using System.Collections.Generic;
-using System.IO;
-class Solution
-{
-    static void Main(String[] args)
-    {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-    }
-}
-
-//Day 7
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
-class Solution
-{
-
-
-
-    static void Main(string[] args)
-    {
-        int n = Convert.ToInt32(Console.ReadLine());
-
-        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
-        ;
-    }
-}
-
-//day 8
-using System;
-using System.Collections.Generic;
-using System.IO;
-class Solution
-{
-    static void Main(String[] args)
-    {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        if (N % 2 == 0)
+        {
+            if (N >= 2 && N <= 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            if (N >= 6 && N <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            if (N > 20)
+            {
+                Console.WriteLine("Not Weird");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Weird");
+        }
     }
 }
