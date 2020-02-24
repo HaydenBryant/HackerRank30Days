04dc62fca64027d9796208bf0b97acda445dcaff
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-02-24 10:54:25 -0500

    Day 9 Done

--- HackerRank30Days/Program.cs
+++ HackerRank30Days/Program.cs
@@ -1,39 +1,39 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
 class Solution
 {
-    static void Main(String[] args)
+
+    // Complete the factorial function below.
+    static int factorial(int n)
     {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-        int inputTimes = Convert.ToInt32(Console.ReadLine());
-        var phoneBook = new Dictionary<string, int>();
+        if (n == 1) return 1;
 
-        for (int i = 0; i < inputTimes; i++)
-        {
-            var directInput = Console.ReadLine();
+        return n * factorial(n - 1);
+    }
 
-            var inputArr = directInput.Split(' ');
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-            phoneBook.Add(inputArr[0], Convert.ToInt32(inputArr[1]));
-        }
+        int n = Convert.ToInt32(Console.ReadLine());
 
-        while (true)
-        {
-            var input = Console.ReadLine();
-            if (String.IsNullOrEmpty(input))
-            {
-                break;
-            }
-            if (phoneBook.ContainsKey(input))
-            {
-                Console.WriteLine("{0}={1}", input, phoneBook[input]);
-            }
-            else
-            {
-                Console.WriteLine("Not found");
-            }
-        }
+        int result = factorial(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
-
